ceb1c33f64a8693f511c6b25d2efade3ca028b3a
toddhsimmons <100725865+toddhsimmons@users.noreply.github.com>
2022-12-18 11:03:28 -0600

    Update get_ap_summary.py

--- 9800 Controller/get_ap_summary.py
+++ 9800 Controller/get_ap_summary.py
@@ -1,13 +1,6 @@
 from netmiko import ConnectHandler
 import json
 
-connect_info = "user_pass.json"
-
-with open(connect_info, 'r') as json_data:
-    args = json.load(json_data)
-
-
-
 device={
     "host": "10.0.0.11",
     "username": "admin",
@@ -15,11 +8,8 @@ device={
     "device_type": "cisco_ios"
 }
 
-
 net_connect = ConnectHandler(**device)
-
 output = net_connect.send_command("show ap summary", read_timeout=20, use_textfsm=True, textfsm_template="cisco_ios_show_ap_summary.template")
-
 net_connect.disconnect()
 
 # Write the Python list to a JSON file
